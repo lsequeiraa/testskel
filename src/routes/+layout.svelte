3cc2de6cea72e7fe6dd8dcd721ec1ba0ee393537
Luís Almeida <17601958+lsequeiraa@users.noreply.github.com>
2024-07-12 22:44:30 +0100

    Add inlang to project.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+	import { i18n } from '$lib/i18n'
+
 	import '../app.css';
 </script>
+<ParaglideJS {i18n}>
+
 
 <slot></slot>
+
+</ParaglideJS>
