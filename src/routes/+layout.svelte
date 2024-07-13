610ab9ea173578899e8623f539f8634255ee768c
Luís Almeida <17601958+lsequeiraa@users.noreply.github.com>
2024-07-13 21:51:10 +0100

    Add basic page layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,30 @@
 <script>
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
-	import { i18n } from '$lib/i18n'
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
 
 	import '../app.css';
+	import Header from './header.svelte';
+	import Sidebar from './sidebar.svelte';
 </script>
+
 <ParaglideJS {i18n}>
-
-
-<slot></slot>
-
+	<div class="grid grid-rows-[auto_1fr_auto]">
+		<!-- Header -->
+		<header class="sticky top-0 z-50">
+			<Header />
+		</header>
+		<!-- Grid Column -->
+		<div class="grid grid-cols-[auto_1fr]">
+			<!-- Sidebar (Left) -->
+			<aside class="sticky top-[56px] h-[calc(100dvh-56px)]"><Sidebar /></aside>
+			<!-- Main -->
+			<main class="overflow-hidden">
+				<slot></slot>
+			</main>
+			<!-- Sidebar (Right) -->
+			<aside class="hidden bg-yellow-500 p-4">(sidebar)</aside>
+		</div>
+		<!-- Footer -->
+		<footer class="hidden bg-blue-500 p-4">(footer)</footer>
+	</div>
 </ParaglideJS>
---
+++ src/routes/header.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import Paperclip from 'lucide-svelte/icons/paperclip';
+	import Calendar from 'lucide-svelte/icons/calendar';
+	import CircleUser from 'lucide-svelte/icons/circle-user';
+	import Menu from 'lucide-svelte/icons/menu';
+</script>
+
+<AppBar
+	headlineClasses="sm:hidden"
+	centerClasses="hidden sm:block"
+	background="bg-surface-900/80 backdrop-blur-sm"
+>
+	{#snippet lead()}
+		<div class="block sm:hidden">
+			<Menu size={20} />
+		</div>
+	{/snippet}
+	<span>HoYoProfile</span>
+	{#snippet trail()}
+		<div class="hidden space-x-4 sm:flex">
+			<Paperclip size={20} />
+			<Calendar size={20} />
+			<CircleUser size={20} />
+		</div>
+		<div class="block sm:hidden">
+			<Menu size={20} />
+		</div>
+	{/snippet}
+</AppBar>
